e65e40c76f040dcbe08d7402388022331b073642
krzysztofz1 <krzysztof.zuk@itdynamics.pl>
2024-04-22 10:40:21 +0200

    Delete github-fetchscripts/10-github-pull-hvscripts.ps1

--- github-fetchscripts/10-github-pull-hvscripts.ps1
+++
@@ -1,3 +0,0 @@
-Write-Host -ForegroundColor Green "Fetching files from GitHub repository... wait a while..."
-cd c:\hv-scripts
-git pull https://4b987df318a293befc5d0c869b0f3b20fe061b3f@github.com/gregorybrzeski/hv-scripts.git
